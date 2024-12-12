1bb68b09ff73bd7ebe95d486de9c2a062a98ef8a
RaphaelStraschewski <r.Straschewski@edu.bbq.de>
2024-12-12 11:58:11 +0100

    Entity Book, Maincontroller und BookFormType

---
+++ src/Controller/BookController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Book;
+use App\Form\BookFormType;
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/book')]
+class BookController extends AbstractController
+{
+    #[Route('/', name: 'app_book_index')]
+    public function index(BookRepository $bookRepository) : Response
+    {
+        return $this->render('book/index.html.twig', ["books" => $bookRepository->findAll()]);
+}
+
+    #[Route('/show/{id}', name: 'app_book_show')]
+    public function show(int $id, BookRepository $bookRepository) : Response
+    {
+        return $this->render('book/show.html.twig', ["book" => $bookRepository->find($id)]);
+    }
+
+    #[Route('/{id}', name: 'app_book_edit')]
+    public function edit(Book $book, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $form = $this->createForm(BookFormType::class, $book);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $book = $form->getData();
+            $entityManager->persist($book);
+            $entityManager->flush();
+            return $this->redirectToRoute('/book/show/{id}', ['id' => $book->getId()]);
+        }
+
+        return $this->render('book/edit.html.twig', ["form" => $form->createView()]);
+    }
+
+    #[Route('/{id}/delete', name: 'app_book_delete')]
+    public function delete(Book $book, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($book);
+        $entityManager->flush();
+        return $this->redirectToRoute('app_book_index');
+    }
+
+    #[Route('/new', name: 'app_book_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $book = new Book();
+        $form = $this->createForm(BookFormType::class, $book, [
+            'action' => $this->generateUrl('app_book_new'),
+            'method' => 'POST'
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $book = $form->getData();
+            $entityManager->persist($book);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_book_show', ['id' => $book->getId()]);
+
+        } else {
+            return $this->render('book/new.html.twig', ["form" => $form->createView()]);
+        }
+    }
+
+
+}
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Book;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -13,11 +12,8 @@ class MainController extends AbstractController
 {
 
     #[Route('/', name: 'app_main_letsgo')]
-    public function letsgo(Book $book, EntityManagerInterface $entityManager, Request $request): Response
+    public function letsgo(Book $book, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm()
         return $this->render('main/letsgo.html.twig');
-
-
     }
 }
